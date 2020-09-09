4043a26469befbb1d57ac1041cb701fb5fcfb571
« ColeenHe » <coleen.hare@gmail.comgit config --global user.name ColeenHegit config --global user.email coleen.hare@gmail.com>
2020-09-09 17:34:08 +0200

    Titre

---
+++ Documents/SUP DE WEB/Pimp my website/portfolio/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = (props) => {
+    return <div>
+            <div>
+                <h1>{props.title}</h1>
+            </div>
+            <div>
+            <p>Mon Portfolio</p>
+            </div>
+    </div>
+}
+
+export default Header
---
+++ Documents/SUP DE WEB/Pimp my website/portfolio/src/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from './Header.js'
+
+const landingpage = () => {
+    return <div>
+        <Header />
+    </div>
+
+}
+export default landingpage
